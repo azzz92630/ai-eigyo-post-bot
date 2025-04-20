5e81c3852fadfeb4d04737809d1475c51270d2e0
azzz92630 <azzz92630@gmail.com>
2025-04-20 16:29:26 +0900

    Add files via upload

---
+++ ai_eigyo_bot/utils/fetch_news.py
@@ -0,0 +1,19 @@
+# utils/fetch_news.py
+import requests
+from bs4 import BeautifulSoup
+
+def get_latest_articles(limit=3):
+    url = "https://news.google.com/rss/search?q=生成AI&hl=ja&gl=JP&ceid=JP:ja"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "xml")
+    
+    items = soup.find_all("item")[:limit]
+    articles = []
+
+    for item in items:
+        articles.append({
+            "title": item.title.text,
+            "link": item.link.text
+        })
+
+    return articles
