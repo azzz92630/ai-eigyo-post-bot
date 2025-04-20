233d4a5745675944dc1d0fbb9be9cd4a4ab52f25
azzz92630 <azzz92630@gmail.com>
2025-04-20 16:54:42 +0900

    Update fetch_news.py

--- utils/fetch_news.py
+++ utils/fetch_news.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 def get_latest_articles(limit=3):
     url = "https://news.google.com/rss/search?q=生成AI&hl=ja&gl=JP&ceid=JP:ja"
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, "xml")
+    soup = BeautifulSoup(response.content, "html.parser")
     
     items = soup.find_all("item")[:limit]
     articles = []
